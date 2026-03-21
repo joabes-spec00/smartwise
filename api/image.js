0198036d43ed8cc13548c7379dcfee29b18913f2
Joabes ferreira <joabesferreirasouza021@gmail.com>
2026-03-21 16:48:25 -0300

    Add image generation API handler

---
+++ api/image.js
@@ -0,0 +1,24 @@
+export default async function handler(req,res){
+
+const response = await fetch("https://api.openai.com/v1/images/generations",{
+
+method:"POST",
+
+headers:{
+"Authorization":`Bearer ${process.env.OPENAI_API_KEY}`,
+"Content-Type":"application/json"
+},
+
+body: JSON.stringify({
+model:"gpt-image-1",
+prompt:req.body.prompt,
+size:"1024x1024"
+})
+
+})
+
+const data = await response.json()
+
+res.json({img:data.data[0].url})
+
+}
